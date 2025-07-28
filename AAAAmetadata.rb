7bf1f5a4058febb07066a112b0287a8def42eb76
tortuguitahack <104583107+tortuguitahack@users.noreply.github.com>
2025-07-28 00:22:31 -0400

    Create AAAAmetadata.rb
    Signed-off-by: tortuguitahack <104583107+tortuguitahack@users.noreply.github.com>

---
+++ AAAAmetadata.rb
@@ -0,0 +1,27 @@
+
+  "search_metadata": {
+    "id": "6886f972ef15ce90b71a309e",
+    "status": "Success",
+    "json_endpoint": "https://serpapi.com/searches/f81e796bb0e06165/6886f972ef15ce90b71a309e.json",
+    "created_at": "2025-07-28 04:15:46 UTC",
+    "processed_at": "2025-07-28 04:15:46 UTC",
+    "amazon_url": "https://www.amazon.com/s?s=exact-aware-popularity-rank&node=All Departments&language=en_US&dc",
+    "raw_html_file": "https://serpapi.com/searches/f81e796bb0e06165/6886f972ef15ce90b71a309e.html",
+    "total_time_taken": 2.84
+  },
+  "search_parameters": {
+    "engine": "amazon",
+    "amazon_domain": "amazon.com",
+    "language": "en_US",
+    "dc": "true",
+    "page": "1",
+    "s": "exact-aware-popularity-rank",
+    "device": "mobile",
+    "node": "All Departments",
+    "shipping_location": "US"
+  },
+  "search_information": {
+    "organic_results_state": "Fully empty"
+  },
+  "error": "Amazon hasn't returned any results for this query."
+}
